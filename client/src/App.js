fa995da9c53ff8162cee07fa26c7da2d60d40e6a
unknown <bmulaw@bu.edu>
2022-02-23 18:15:49 -0500

    moved firebase config to firebaseConfig.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
-import { initializeApp } from "firebase/app";
-import firebaseConfig from './firebaseConfig';
 
 export default function App() {
-  const app = initializeApp(firebaseConfig);
   return (
     <div className="App">
       
