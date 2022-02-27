fca27cdbfe32d556d4d4c502e54b83164caca61a
unknown <bmulaw@bu.edu>
2022-02-26 20:34:23 -0500

    deleted unnecessary react templated files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import {app, db, firebaseConfig} from './firebaseConfig';
+import firebase from 'firebase'; 
 
 export default function App() {
+  const firebaseapp = firebase.apps[0];
   return (
     <div className="App">
-      
+      <code>
+        <pre>
+          {JSON.stringify(firebaseapp.options, null, 2)}
+        </pre>
+      </code>
     </div>
   );
 }
