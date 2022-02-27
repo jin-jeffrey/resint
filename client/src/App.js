6fa0d7549ab677a71642981971bbd1b955eef20e
unknown <bmulaw@bu.edu>
2022-02-26 21:50:15 -0500

    press button and app will display all users signed up and in firebase database

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,43 @@
-import './App.css';
-import {app, db, firebaseConfig} from './firebaseConfig';
-import firebase from 'firebase'; 
+import { useState, useEffect } from 'react';
+import { db } from './firebaseConfig';
+import { collection, getDocs } from 'firebase/firestore';
 
 export default function App() {
-  const firebaseapp = firebase.apps[0];
+
+  const [users, setUsers] = useState([])
+  const [display, setDisplay] = useState(false);
+  const [usersToDisplay, setUsersToDisplay] = useState([])
+
+   const getUsers = async (db) => {
+    const usersCollection = collection(db, 'users');
+    const usersSnapshot = await getDocs(usersCollection);
+    const usersList = usersSnapshot.docs.map(doc => doc.data());
+    return usersList;
+  }
+
+  useEffect(() => {
+    if (display) {
+      getUsers(db).then(res => setUsers(res))
+      const usersList = users.map((user) => 
+        <li key={user.name}>
+          {user.name}
+        </li>
+      )
+      setUsersToDisplay(usersList);
+    }
+  }, [display])
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setDisplay(!display)
+  }
+
   return (
     <div className="App">
-      <code>
-        <pre>
-          {JSON.stringify(firebaseapp.options, null, 2)}
-        </pre>
-      </code>
+      <form onSubmit={handleSubmit}>
+        <button type="submit">See Users</button>
+      </form>
+      {display?<ul>{usersToDisplay}</ul> :null}
     </div>
   );
 }
