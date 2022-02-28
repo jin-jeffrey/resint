6964a6cfbbb283aea2155c4314ad73e1a74c6f55
Jeffrey Jin <jinjeff2@gmail.com>
2022-02-28 01:18:47 -0500

    added routes with connected firebase

--- resint-backend/index.js
+++ resint-backend/index.js
@@ -1,12 +1,43 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+import firebaseConfig from './firebaseConfig.js';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { initializeApp}  from "firebase/app";
+import { getFirestore, collection, getDocs, addDoc, deleteDoc } from "firebase/firestore";
+import dotenv from "dotenv";
 
+// initialize app with express, cors, and env variables
 const app = express();
+dotenv.config();
 app.use(cors());
+var jsonParser = bodyParser.json()
 
-app.get('/', (req,res) => {
-    res.send('test');
+// connect to firebase 
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+// ROUTES
+
+// get apps by user
+app.get('/apps', jsonParser, async (req,res) => {
+    const appsCollection = collection(db, 'apps');
+    const appsSnapshot = await getDocs(appsCollection);
+    const appsList = appsSnapshot.docs.map((doc) => {
+        if (doc.data().user_id == req.body.user_id) {
+            return doc.data();
+        }
+    });
+    res.send(appsList);
+})
+
+// add new app
+app.post('/newApp', jsonParser, async (req, res) => {
+    try {
+        const docRef = await addDoc(collection(db, "apps"), req.body);
+        res.status(200).send("Document written with ID: " + docRef.id);
+    } catch (e) {
+        res.status(400).send("Failed to add document");
+    }      
 })
 
 app.listen(process.env.PORT, () =>
