4195794ef773aa8a5bfb8fa4bc7987382c2e64e2
Jeffrey Jin <jinjeff2@gmail.com>
2022-03-21 15:54:47 -0400

    fixed chrome extension installation errors

--- chrome-ext/index.js
+++ chrome-ext/index.js
@@ -1,5 +1,9 @@
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Resint installed");
+});
+
 function saveApplication() {
-    const url = 'localhost:8000/addApps';
+    const url = 'http://localhost:8000/addApps';
     const options = {
         method: "POST",
         headers: {
@@ -10,15 +14,17 @@ function saveApplication() {
             company: document.getElementById('company'),
             position: document.getElementById('position'),
             description: document.getElementById('description'),
-            date: document.getElementById('date')
+            date: document.getElementById('date'),
+            user_id: 'djsaodoa'
         })
     };
     fetch(url, options)
     .then((response) => response.json())
     .then((data) => {
+        console.log("TEST");
         console.log(data);
     })
 }
 
-const form = document.getElementById('add-app-form');
+const form = document.getElementById('add-form');
 form.addEventListener('submit', saveApplication);
