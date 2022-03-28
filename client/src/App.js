5eca051af52db273b663644eb43092b46f6901cb
kolade2 <koladea@bu.edu>
2022-03-28 15:52:14 -0400

    frntend

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,11 @@ import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import Applications from "./components/Applications";
 import history from './history';
-import JobApps from './components/JobApps'
-
+import JobApps from './components/JobApps';
+import MainHero from './components/MainHero';
+import Product from "./components/Product";
+import Features from "./components/Features";
+import Header from "./components/Header";
 
 function App() {
 
@@ -13,6 +16,7 @@ function App() {
     <div className="app">
       <Router history={history}>
         <Routes>
+          <Route exact path="/" element={[<Header />, <MainHero />, <Product />, <Features />]} />
           <Route exact path="/login" element={<Login />} />
           <Route exact path="/dashboard" element={<Dashboard />} />
           <Route exact path="/resint" element={<JobApps />} />
