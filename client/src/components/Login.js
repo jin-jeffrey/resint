d728d44038700546eb0ce6bfa664e79e74a863f7
kolade2 <koladea@bu.edu>
2022-04-02 23:57:20 -0400

    updated frontend

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -21,16 +21,16 @@ function Login() {
   return (
     <div className="login">
       <div className="navbar">
-        <img src="" className="logo"></img>
+        <img src='../../public/images/icons8-tree-64.png' className="logo"></img>
         <ul>
           <li><a href="#">About us</a></li>
           <li><a href="#">Meet the team</a></li>
         </ul>
       </div>
       <div className="content">
-        <h1>RESINT!</h1> <h1>Your guide to landing your dream career.</h1>
+        <h1>RESINT!</h1> <h1>Your guide to <span class="text">landing</span> your <span class="text">dream career.</span></h1>
         <div>
-        <button className="login__btn" onClick={signInWithGoogle}><span></span> Login </button>
+          <button className="login__btn" onClick={signInWithGoogle}><span class="login_btn_span"></span> Login </button>
         </div>
       </div>
     </div>
