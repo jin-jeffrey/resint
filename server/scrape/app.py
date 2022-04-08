5caf4dd89dade275e85f1c947f099a947b9bb8f3
unknown <bmulaw@bu.edu>
2022-04-07 22:05:37 -0400

    added instructions on how to run app

--- server/scrape/app.py
+++ server/scrape/app.py
@@ -5,13 +5,17 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+# run this python code and
+# paste the link below into your browser 
+# http://127.0.0.1:5000/applied?url=https://www.linkedin.com/jobs/view/3009127843/?alternateChannel=search&refId=KkR88ziDVuOz%2Fb%2BjwiTlDQ%3D%3D&trackingId=nbFUVJd6A3UOCm0%2F55MzNA%3D%3D&trk=d_flagship3_jobs_discovery_jymbii&uid=f4vwSMeNe1b0T2nrp3jHkIpYoEO2
+
 @app.route('/applied', methods=["GET"])
 def main():
     try:
         data = {}
         url = request.args.get('url')
-        # uid = request.args.get('uid')
-        # data['Uid'] = uid
+        uid = request.args.get('uid')
+        data['Uid'] = str(uid)
         data["Link"] = str(url)
         data["Status"] = "Applied"
         
@@ -41,7 +45,5 @@ def main():
         print(e, '404')
 
     
-
-# main()
 if __name__ == '__main__':
     app.run(debug=True)
