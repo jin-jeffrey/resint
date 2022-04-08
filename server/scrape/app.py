ccc124904b7e2be5fe9d8c211b164566ca53deaf
unknown <bmulaw@bu.edu>
2022-04-08 01:02:29 -0400

    added firebase config and flask now can add data to firebase with just url and uid

--- server/scrape/app.py
+++ server/scrape/app.py
@@ -2,26 +2,35 @@ import requests
 from bs4 import BeautifulSoup
 from datetime import date
 from flask import Flask, request
+from firebase_admin import credentials, firestore, initialize_app
 
-app = Flask(__name__)
+app=Flask(__name__)
+# Firebase initialization
+cred = credentials.Certificate("./serviceAccountKey.json")
+default_app = initialize_app(cred)
+db = firestore.client()
+apps_ref = db.collection('apps')
 
 # run this python code and
 # paste the link below into your browser 
 # http://127.0.0.1:5000/applied?url=https://www.linkedin.com/jobs/view/3009127843/?alternateChannel=search&refId=KkR88ziDVuOz%2Fb%2BjwiTlDQ%3D%3D&trackingId=nbFUVJd6A3UOCm0%2F55MzNA%3D%3D&trk=d_flagship3_jobs_discovery_jymbii&uid=f4vwSMeNe1b0T2nrp3jHkIpYoEO2
 
-@app.route('/applied', methods=["GET"])
+@app.route('/applied', methods=["POST"])
 def main():
+    url = request.args.get('url')
+    uid = request.args.get('uid')
+    if url == None or uid == None:
+        return {"Error": "Please pass in url and uid"}, 404
+
     try:
-        data = {}
-        url = request.args.get('url')
-        uid = request.args.get('uid')
-        data['Uid'] = str(uid)
-        data["Link"] = str(url)
-        data["Status"] = "Applied"
-        
+        data = {
+            "Uid": str(uid),
+            "Link": str(url),
+            "Status":"Applied"
+        }
+
         r = requests.get(url)
         soup = BeautifulSoup(r.text, "html.parser")
-        
 
         # get Company Name
         company_name_class_name = "topcard__org-name-link topcard__flavor--black-link"
@@ -38,7 +47,9 @@ def main():
         # get Applied Date
         today = date.today()
         data['Date'] = today.strftime("%Y-%m-%d")
-        
+
+        # Add to Firestore DB
+        apps_ref.add(data)
         return dict(data)
     
     except Exception as e:
