b295065beb305b6a6af3c9508abc6a70d795ad36
Jeffrey Jin <jinjeff2@gmail.com>
2022-04-25 15:54:01 -0400

    components functional with add, edit, delete, and pagination

--- client/src/components/Applications.js
+++ client/src/components/Applications.js
@@ -7,17 +7,16 @@ import "./Applications.css"
 import addbutton from './icons/add.png';
 import editbutton from './icons/edit.png';
 import deletebutton from './icons/delete.png';
-import { allData } from "./constants";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, logout } from "../firebase";
 import EditModal from './EditModal.js';
+import Modal from './Modal.js';
 
 const tableHead = {
   CompanyName: "Company",
   JobTitle: "Position",
-  CompanyDescription: "Description",
+  // CompanyDescription: "Description",
   JobLocation: "Location",
-  Notes: "Notes",
   Date: "Date",
   Action: "Actions"
 };
@@ -26,26 +25,61 @@ const Table = () => {
   const countPerPage = 10;
   const [value, setValue] = React.useState("");
   const [currentPage, setCurrentPage] = React.useState(1);
-  const [collection, setCollection] = React.useState(
-    cloneDeep(allData.slice(0, countPerPage))
-  );
+  const [collection, setCollection] = React.useState([]);
   const [viewOpen, setViewOpen] = React.useState(false);
   const [editOpen, setEditOpen] = React.useState(false);
+  const [isOpen, setIsOpen] = React.useState(false);
   const [user, loading, error] = useAuthState(auth);
+  const [loaded, setLoaded] = React.useState(true);
   const [app, setApp] = React.useState({});
+  const [applications, setApplications] = React.useState([]);
 
   const searchData = React.useRef(
     throttle(val => {
       const query = val.toLowerCase();
       setCurrentPage(1);
       const data = cloneDeep(
-        allData
+        applications
           .filter(application => application.CompanyName.toLowerCase().indexOf(query) > -1)
           .slice(0, countPerPage)
       );
       setCollection(data);
     }, 400)
   );
+
+  React.useEffect(() => {
+    if(user){
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        var raw = JSON.stringify({
+            "Uid": user?.uid
+        });
+
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        fetch("https://resint.herokuapp.com/getApps", requestOptions)
+            .then(response => response.json())
+            .then(result => {
+              let res = result;
+              if (result.length < 10) {
+                for (let i = result.length; i < 10; i++) {
+                  res.push({});
+                }
+              }
+              setApp(res);
+              setApplications(res);
+              setCollection(cloneDeep(res.slice(0, countPerPage)));
+              setLoaded(true);
+            })
+            .catch(error => console.log('error', error));
+        }
+}, [user])
+
 
   React.useEffect(() => {
     if (!value) {
@@ -59,7 +93,7 @@ const Table = () => {
     setCurrentPage(p);
     const to = countPerPage * p;
     const from = to - countPerPage;
-    setCollection(cloneDeep(allData.slice(from, to)));
+    setCollection(cloneDeep(applications.slice(from, to)));
   };
 
   const tableRows = rowData => {
@@ -67,6 +101,9 @@ const Table = () => {
     const tableCell = Object.keys(tableHead);
     const columnData = tableCell.map((keyD, i) => {
       if (keyD == "Action") {
+        if (key.CompanyName == null) {
+          return <td key={i}></td>;
+        }
         return (
         <td key={i}>
           <button onClick={(event) => editOpened(event, key)}><img src={editbutton}/></button>
@@ -74,9 +111,11 @@ const Table = () => {
         </td>
         )
       } else if (keyD == "CompanyName") {
-        return (<td key={i}><a href={key.Link} target="_blank">{key[keyD]}</a></td>)
+        return (<td className="Company" key={i}><a href={key.Link} target="_blank">{key[keyD]}</a></td>)
+      } else if (keyD == "Date") {
+        return (<td className="Date" key={i}>{updateDate(key[keyD])}</td>)
       } else {
-        return <td key={i}>{key[keyD]}</td>;
+        return <td className={keyD} key={i}>{key[keyD]}</td>;
       }
     });
     return <tr key={index}>{columnData}</tr>;
@@ -88,9 +127,20 @@ const Table = () => {
 
   const headRow = () => {
     return Object.values(tableHead).map((title, index) => (
-      <td key={index}>{title}</td>
+      <td className={title} key={index}>{title}</td>
     ));
   };
+
+  function updateDate(date) {
+    if (date == null) {
+      return "";
+    } else {
+      let year = date.substring(0,4);
+      let month = date.substring(5,7);
+      let day = date.substring(8,10);
+      return month + "/" + day + "/" + year;
+    }
+  }
 
   function deleteApplication(e, did) {
     e.preventDefault();
@@ -125,27 +175,39 @@ const Table = () => {
 
   return (
     <>
-      <div class="search">
-        <input
-          placeholder="Search Applications"
-          value={value}
-          onChange={e => setValue(e.target.value)}
-        />
-      </div>
-      <table>
-        <thead>
-          <tr>{headRow()}</tr>
-        </thead>
-        <tbody className="trhover">{tableData()}</tbody>
-      </table>
-      <Pagination
-        className="pagination"
-        pageSize={countPerPage}
-        onChange={updatePage}
-        current={currentPage}
-        total={allData.length}
-      />
-      {editOpen && <EditModal open={editOpen} onClose={() => setEditOpen(false)} userid={user?.uid} app={app}/>}
+      { loaded &&
+        <>
+        <div className="box">
+          <button onClick={() => setIsOpen(true)}><img className="add-button" src={addbutton}/></button>
+          <div className="search">
+            <input
+              className="search-input"
+              placeholder="Search Applications"
+              value={value}
+              onChange={e => setValue(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <table className="application-table">
+          <thead>
+            <tr>{headRow()}</tr>
+          </thead>
+          <tbody className="trhover">{tableData()}</tbody>
+        </table>
+        <div className="box">
+          <Pagination
+            className="pagination"
+            pageSize={countPerPage}
+            onChange={updatePage}
+            current={currentPage}
+            total={applications.length}
+          />
+        </div>
+        {editOpen && <EditModal open={editOpen} onClose={() => setEditOpen(false)} userid={user?.uid} app={app}/>} 
+        <Modal open={isOpen} onClose={() => setIsOpen(false)} userid={user?.uid} />
+        </>  
+      }
     </>
   );
 };
